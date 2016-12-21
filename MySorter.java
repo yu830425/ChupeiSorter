d5cb6f73d6e2777ed61b6f10fc87c1caa12b7496
Chupei <yu830425@gmail.com>
2016-12-21 12:38:50 +0800

    Final Edition

--- MySorter.java
+++ MySorter.java
@@ -2,8 +2,22 @@ package ntou.cs.sorter;
 
 public class MySorter
 {
+	private boolean validation(double[] d)
+	{		
+		for(int i=0;i<d.length;i++)
+		{
+			if(d[i]<0)
+				return true;
+		}
+		
+		return false;
+	}
+	
 	public void sort (double[] d)
 	{
+		if(validation(d))
+			return ;
+		
 		for (int i = 0; i < d.length - 1; i++)
 		{
 			for (int j = 0; j < d.length - i - 1; j++)
